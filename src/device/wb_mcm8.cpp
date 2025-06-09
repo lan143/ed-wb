1f3860438ca8e118d5a3768ef347febe6a6ba307
Artem Kravchenko <lan143@lan143.ru>
2025-06-09 08:40:04 +0300

    Add methods for get switch modes count

--- src/device/wb_mcm8.cpp
+++ src/device/wb_mcm8.cpp
@@ -27,3 +27,47 @@ bool MCM8::setInputMode(uint8_t pin, uint16_t mode)
 
     return _client.holdingRegisterWrite(_address, 0x0009 + pin, mode);
 }
+
+uint16_t MCM8::getShortPressCount(uint8_t pin)
+{
+    pin--;
+
+    if (pin > 7) {
+        return 0;
+    }
+
+    return _client.inputRegisterRead(_address, 0x01D0 + pin);
+}
+
+uint16_t MCM8::getLongPressCount(uint8_t pin)
+{
+    pin--;
+
+    if (pin > 7) {
+        return 0;
+    }
+
+    return _client.inputRegisterRead(_address, 0x01E0 + pin);
+}
+
+uint16_t MCM8::getDoublePressCount(uint8_t pin)
+{
+    pin--;
+
+    if (pin > 7) {
+        return 0;
+    }
+
+    return _client.inputRegisterRead(_address, 0x01F0 + pin);
+}
+
+uint16_t MCM8::getShortAndThenLongPressCount(uint8_t pin)
+{
+    pin--;
+
+    if (pin > 7) {
+        return 0;
+    }
+
+    return _client.inputRegisterRead(_address, 0x0200 + pin);
+}
